755d95568a2bdcb223642a5a9f270a2cb69ed75a
ramoscariel <103594586+ramoscariel@users.noreply.github.com>
2023-12-05 23:24:31 -0500

    Add project files.

---
+++ Shared/Empresa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIEmpresas.Shared
+{
+    public class Empresa
+    {
+        public int Codigo { get; set; }
+        public string NombreEmisor {  get; set; }
+        public string Ruc {  get; set; }
+    }
+}
